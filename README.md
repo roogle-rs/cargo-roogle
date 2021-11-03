791ce18ce2be840823d5b976227d0d1955d7f396
Hirochika Matsumoto <git@hkmatsumoto.com>
2021-11-03 17:48:03 +0900

    feat: Implement basic features

---
+++ README.md
@@ -0,0 +1,2 @@
+# cargo-roogle
+A Cargo subcommand to run [Roogle](https://github.com/hkmatsumoto/roogle) on local crates.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::{self, create_dir_all};
+use std::process;
+
+use anyhow::{Context, Result};
+use cargo_metadata::MetadataCommand;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(bin_name = "cargo")]
+enum Command {
+    Roogle(Opt),
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {}
+
+fn build(_: &Opt) -> Result<()> {
+    process::Command::new("cargo")
+        .args(&[
+            "+nightly",
+            "rustdoc",
+            "--",
+            "--output-format=json",
+            "-Z",
+            "unstable-options",
+        ])
+        .spawn()
+        .context("failed to build search index using `rustdoc`")?
+        .wait()?;
+
+    let metadata = MetadataCommand::new()
+        .no_deps()
+        .exec()
+        .context("failed to get crate's metadata")?;
+    let members = metadata
+        .packages
+        .iter()
+        .filter(|p| metadata.workspace_members.contains(&p.id))
+        .map(|p| &p.name);
+
+    create_dir_all("target/roogle/crate").context("failed create `crate` directory")?;
+    for member in members {
+        fs::rename(
+            format!("target/doc/{}.json", member),
+            format!("target/roogle/crate/{}.json", member),
+        )
+        .context("failed to store crate's index to index folder")?;
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let Command::Roogle(opt) = Command::from_args();
+    build(&opt)?;
+
+    process::Command::new("roogle")
+        .args(&["--index", "target/roogle"])
+        .spawn()
+        .context("failed to start server")?
+        .wait()?;
+
+    Ok(())
 }
